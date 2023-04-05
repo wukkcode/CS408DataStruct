98e56e88fd41c1e8f136e616a9ee5bd643debdc1
wukkcode <51038795+wukkcode@users.noreply.github.com>
2023-04-05 21:11:56 +0800

    Update 单链表结构定义及基本操作.cpp

---
+++ 线性表/链表/单链表结构定义及基本操作.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElemType;
+
+typedef struct LinkNode
+{
+    ElemType data;
+    struct LinkNode* next;
+} LinkNode, *LinkList;
+
+// 初始化单链表
+
+bool InitLinkList(LinkList &LL)
+{
+    
+}
+
+int main()
+{
+    LinkList LL;
+}
+
+

3e650d33b548d8fb893b3c2f713bd67debe897ea
wukkcode <51038795+wukkcode@users.noreply.github.com>
2023-04-06 09:53:38 +0800

    Update 单链表结构定义及基本操作.cpp

--- 线性表/链表/单链表结构定义及基本操作.cpp
+++ 线性表/链表/单链表结构定义及基本操作.cpp
@@ -93,7 +93,7 @@ bool LinkListInsertFromTail(LinkList LL)
 }
 
 // 按位置查找
-bool LinkListSearchByPos(LinkList LL, int pos ,ElemType &node_value)
+bool LinkListSearchByPos(LinkList LL, int pos , LinkNode* &target_node)
 {
     // 判断链表是否为空
     if (LinkListIsEmpty(LL) == true)
@@ -101,19 +101,19 @@ bool LinkListSearchByPos(LinkList LL, int pos ,ElemType &node_value)
         return false;
     }
 
-    // 判断查找位置是否合理
-    if (pos < 1)
+    // 判断查找位置是否合理(为了后序操作，可以查找第0个节点，即头节点)
+    if (pos < 0)
     {
         return false;
     }
-    LinkNode* current_node = LL->next;
-    int count = 1;
+    LinkNode* current_node = LL;
+    int count = 0;
     while (current_node != NULL && count < pos)
     {
         current_node = current_node->next;
         count++;
     }
-    node_value = current_node->data;
+    target_node = current_node;
     return true;
 }
 
@@ -141,6 +141,80 @@ bool LinkListSearchByValue(LinkList LL, ElemType node_value, int &pos)
     return true;
 }
 
+// 在链表特定的位置插入节点
+bool LinkListInsertedAtPos(LinkList LL, int pos, ElemType node_value)
+{
+    // 判断链表是否为空
+    if (LinkListIsEmpty(LL) == true)
+    {
+        return false;
+    }
+    // 判断插入位置是否合理
+    if (pos < 1)
+    {
+        return false;
+    }
+    // 找到要插入节点的前一个节点
+    LinkNode* pre_node;
+    LinkListSearchByPos(LL, pos-1, pre_node);
+    LinkNode* inserted_node = (LinkNode *)malloc(sizeof(LinkNode));
+    inserted_node->data = node_value;
+    inserted_node->next = pre_node->next;
+    pre_node->next = inserted_node;
+    return true;
+}
+
+// 在特定的位置删除节点
+bool LinkListDeleteAtPos(LinkList LL, int pos, ElemType &node_value)
+{
+    // 判断链表是否为空
+    if (LinkListIsEmpty(LL) == true)
+    {
+        return false;
+    }
+    // 判断删除位置是否合理
+    if (pos < 1)
+    {
+        return false;
+    }
+    // 找到被删除节点的前一个位置
+    LinkNode* pre_node;
+    LinkListSearchByPos(LL, pos - 1, pre_node);
+    LinkNode* free_node = pre_node->next;
+    pre_node->next = free_node->next;
+    node_value = free_node->data;
+    free(free_node);
+    return true;
+}
+
+// 快速找到链表的中间节点，这个算法真的能给人带来很多感想，注意体会(考试常考)
+bool QuickFindMidNode(LinkList LL, LinkNode* &target_node)
+{
+    // 判断链表是否为空
+    if (LinkListIsEmpty(LL) == true)
+    {
+        return false;
+    }
+    // 用双指针法
+    LinkNode* one_step_node = LL->next;
+    LinkNode* two_steps_node = LL->next;
+    while (two_steps_node != NULL)
+    {
+        two_steps_node = two_steps_node->next;
+        if (two_steps_node == NULL)
+        {
+            break;
+        }
+        two_steps_node = two_steps_node->next;
+        if (two_steps_node != NULL)
+        {
+            one_step_node = one_step_node->next;
+        }
+    }
+    target_node = one_step_node;
+    return true;
+}
+
 int main()
 {
     LinkList LL;
@@ -148,11 +222,19 @@ int main()
 //    LinkListInsertFromHead(LL);
     LinkListInsertFromTail(LL);
     TraverseLinkList(LL);
-    ElemType node_value;
-    LinkListSearchByPos(LL, 4, node_value);
-    printf("node_value = %d\n", node_value);
-    int pos;
+    LinkNode* target_node;
+    LinkListSearchByPos(LL, 4, target_node);
+    printf("node_value = %d\n", target_node->data);
+    int pos = 0;
     LinkListSearchByValue(LL, 9, pos);
     printf("position = %d\n", pos);
+    LinkListInsertedAtPos(LL, 1, 9);
+    TraverseLinkList(LL);
+    ElemType node_value;
+    LinkListDeleteAtPos(LL, 1, node_value);
+    printf("node value = %d\n", node_value);
+    TraverseLinkList(LL);
+    QuickFindMidNode(LL, target_node);
+    printf("Mid node = %d\n", target_node->data);
     return 0;
 }

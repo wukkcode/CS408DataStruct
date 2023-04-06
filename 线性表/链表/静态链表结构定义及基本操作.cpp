d0e62b43bdf3bef886e6213edacc55383e3f499f
wukkcode <51038795+wukkcode@users.noreply.github.com>
2023-04-06 12:08:00 +0800

    Create 静态链表结构定义及基本操作.cpp

---
+++ 线性表/链表/静态链表结构定义及基本操作.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MaxSize 10
+
+typedef int ElemType;
+typedef struct StaticLinkList
+{
+    ElemType data;
+    int next;
+} StaticLinkList[MaxSize];
+
+bool InitStaticLinkList(StaticLinkList SLL)
+{
+    // 头节点指针域设置为-1来表示静态链表为空
+    SLL[0].next = -1;
+
+    // 将其他指针域设置成特殊值，以便后序操作
+    for (int i = 1; i < MaxSize; i++)
+    {
+        SLL[i].next = -2;
+    }
+    return true;
+}
+

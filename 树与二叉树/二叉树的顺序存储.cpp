af966ebe8001bd84637f8bc200ef2dffc0613f33
wukkcode <51038795+wukkcode@users.noreply.github.com>
2023-04-14 23:31:35 +0800

    Update 二叉树的顺序存储.cpp

--- 树与二叉树/二叉树的顺序存储.cpp
+++ 树与二叉树/二叉树的顺序存储.cpp
@@ -8,7 +8,7 @@ typedef struct
 {
     ElemType data;
     bool empty_status;
-} SequenceBinaryTree[MaxSize+1];
+} SBTNode,SequenceBinaryTree[MaxSize+1];
 
 // 初始化顺序存储的二叉树
 void InitSequenceBinaryTree(SequenceBinaryTree &SBT)
@@ -17,6 +17,52 @@ void InitSequenceBinaryTree(SequenceBinaryTree &SBT)
     {
         SBT[i].empty_status = true;
     }
+}
+
+// 找节点的左孩子
+bool FindLeftChildNode(SequenceBinaryTree SBT,int node_index, int number, SBTNode &SBTN)
+{
+    // 判断完全二叉树是否有左孩子
+    if (2 * node_index > number)
+    {
+        return false;
+    }
+    SBTN = SBT[node_index * 2];
+    return true;
+}
+
+// 找节点的右孩子
+bool FindRightChildNode(SequenceBinaryTree SBT,int node_index, int number, SBTNode &SBTN)
+{
+    // 判断完全二叉树是否有右孩子
+    if (2 * node_index + 1 > number)
+    {
+        return false;
+    }
+    SBTN = SBT[node_index * 2 + 1];
+    return true;
+}
+
+// 找节点的父节点
+bool FindFatherNode(SequenceBinaryTree SBT, int node_index, int number, SBTNode &SBTN)
+{
+    // 判断节点node_index合不合理
+    if (node_index == 1 || node_index > number)
+    {
+        return false;
+    }
+    SBTN = SBT[node_index / 2];
+    return true;
+}
+
+// 判断节点是否是叶子节点/分支节点
+bool SBTNodeIsLeafNode(SequenceBinaryTree SBT, int node_index, int number)
+{
+    if (node_index > number / 2)
+    {
+        return true; // 是叶子节点
+    }
+    return false; // 不是叶子节点
 }
 
 int main()
@@ -28,6 +74,6 @@ int main()
         SBT[i].data = i;
         SBT[i].empty_status = false;
     } 
-    
+
     return 0;
 }

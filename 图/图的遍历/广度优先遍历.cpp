5ed72283da63e3402146077419a77d328d5e16c1
wukkcode <51038795+wukkcode@users.noreply.github.com>
2023-04-27 09:15:54 +0800

    Update 广度优先遍历.cpp

--- 图/图的遍历/广度优先遍历.cpp
+++ 图/图的遍历/广度优先遍历.cpp
@@ -4,30 +4,27 @@
 
 bool visit_status[MaxVertexNum];
 
-VertexNode* FindFirstNeighbor(AdjacencyListGraph ALQ, VertexNode *current_vertex);
-VertexNode* FindNextNeighbor(AdjacencyListGraph ALQ, VertexNode *current_vertex, VertexNode* neighbor_vertex);
+int FindFirstNeighbor(AdjacencyListGraph ALQ, int current_vertex_index);
+int FindNextNeighbor(AdjacencyListGraph ALQ, int current_vertex_index, int neighbor_vertex_index);
 
-void VistVertex(VertexNode* current_vertex)
-{
-    printf("%c", current_vertex->data);
-}
+void VistVertex(int current_vertex_index);
 
-void BreadthFirstSearch(AdjacencyListGraph ALG, int vertex_index, LinkListQueue LLQ)
+
+void BreadthFirstSearch(AdjacencyListGraph ALG, int current_vertex_index, LinkListQueue LLQ)
 {
-    VistVertex(&ALG.vertex_nodes[vertex_index]);
-    visit_status[vertex_index] = true;
-    EnLinkListQueue(LLQ, &ALG.vertex_nodes[vertex_index]);
-    VertexNode *current_vertex = NULL;
+    VistVertex(current_vertex_index);
+    visit_status[current_vertex_index] = true;
+    EnLinkListQueue(LLQ, current_vertex_index);
     while (LinkListQueueIsEmpty(LLQ) == false)
     {
-        DeLinkListQueue(LLQ, current_vertex);
-        for (VertexNode *neighbor_vertex = FindFirstNeighbor(ALG, current_vertex); neighbor_vertex != NULL; neighbor_vertex = FindNextNeighbor(ALG, current_vertex, neighbor_vertex))
+        DeLinkListQueue(LLQ, current_vertex_index);
+        for (int neighbor_vertex_index = FindFirstNeighbor(ALG, current_vertex_index); neighbor_vertex_index > -1; neighbor_vertex_index = FindNextNeighbor(ALG, current_vertex_index, neighbor_vertex_index))
         {
-            if (visit_status[neighbor_vertex->vertex_index] == false)
+            if (visit_status[neighbor_vertex_index] == false)
             {
-                VistVertex(neighbor_vertex);
-                visit_status[neighbor_vertex->vertex_index] = true;
-                EnLinkListQueue(LLQ, neighbor_vertex);
+                VistVertex(neighbor_vertex_index);
+                EnLinkListQueue(LLQ, neighbor_vertex_index);
+                visit_status[neighbor_vertex_index] = true
             }
         }
     }
@@ -41,11 +38,11 @@ void BreadthFirstSearchTraverse(AdjacencyListGraph ALG)
     }
     LinkListQueue LLQ;
     InitLinkListQueue(LLQ);
-    for (int vertex_index = 0; vertex_index < ALG.vertex_num; vertex_index++)
+    for (int current_vertex_index = 0; current_vertex_index < ALG.vertex_num; current_vertex_index++)
     {
-        if (visit_status[vertex_index] == false)
+        if (visit_status[current_vertex_index] == false)
         {
-            BreadthFirstSearch(ALG, vertex_index, LLQ);
+            BreadthFirstSearch(ALG, current_vertex_index, LLQ);
         }
     }
 }

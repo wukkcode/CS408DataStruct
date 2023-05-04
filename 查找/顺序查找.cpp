2baef0cfd7cf4b60ffdf97f29a857234238750bc
wukkcode <51038795+wukkcode@users.noreply.github.com>
2023-05-04 17:14:57 +0800

    Create 顺序查找.cpp

---
+++ 查找/顺序查找.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElemType;
+typedef struct
+{
+    ElemType *data;
+    int length;
+}DynamicSequenceList;
+
+bool InitDynamicSequenceList(DynamicSequenceList &DSL, int length)
+{
+    DSL.length = length + 1;
+    DSL.data = (ElemType *)malloc(DSL.length * sizeof(ElemType));
+    if (DSL.data == NULL)
+    {
+        return false;
+    }
+    for (int i = 1; i < DSL.length; i++)
+    {
+        DSL.data[i] = i;
+    }
+    return true;
+}
+
+// 查找操作
+int SearchSequenceList(DynamicSequenceList DSL, int element_value)
+{
+    DSL.data[0] = element_value; // 存储哨兵
+    int pos_index;
+    for (pos_index = DSL.length-1; DSL.data[pos_index] != element_value; pos_index--);
+    return pos_index;
+}
+
+int main()
+{
+    DynamicSequenceList DSL;
+    InitDynamicSequenceList(DSL, 10);
+    int pos = SearchSequenceList(DSL, 0);
+    if (pos != 0)
+    {
+        printf("SequenceSearch is Successful!");
+    }
+    else
+    {
+        printf("SequenceSearch is Failed");
+    }
+    return 0;
+}
